05562df9491524503d751b85979f7a43ba88bb1b
martin4mari <martinmariaaa@gmail.com>
2022-08-02 03:24:56 -0300

    v03

--- src/components/DarkTheme.vue
+++ src/components/DarkTheme.vue
@@ -9,20 +9,13 @@ const emits = defineEmits({
 </script>
 
 <template>
-  <div :class="active ? '' : 'dark'">
+  <div>
     <button
       @click="emits('onChange', active), (active = !active)"
-      class="hidden dark:dark light button absolute z-20 mr-5 bg-gray-100 border-gray-500/50 right-0 rounded-b-full lg:block p-[1px] pt-[1px]"
+      class="dark:dark light absolute z-40 mr-5 bg-gray-100 border-gray-300 dark:border-gray-600 right-24 lg:right-0 rounded-b-full lg:block p-[1px] pt-[1px] border-[1px]"
     >
       <img v-show="!active" src="../components/icons/sun.svg" class="w-4 h-4" alt="" />
       <img v-show="active" src="../components/icons/lmoon.svg" class="w-4 h-4" alt="" />
     </button>
   </div>
 </template>
-
-<style>
-.button {
-  border: solid 1px;
-  border-top: none;
-}
-</style>
